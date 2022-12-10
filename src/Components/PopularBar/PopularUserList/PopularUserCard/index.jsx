5c02d7f7dc826300fa9bb3cb46cbf1c88c627d25
yhosutun2491 <yhosutun2491@gmail.com>
2022-12-10 22:32:38 +0800

    refactor- use fakeuser data to render PopularUserCard

--- src/Components/PopularBar/PopularUserList/PopularUserCard/index.jsx
+++ src/Components/PopularBar/PopularUserList/PopularUserCard/index.jsx
@@ -1,17 +1,20 @@
 import styles from "./PopularUserCard.module.scss";
 import { ReactComponent as UserFakeAvatar } from "../../../../assets/icons/AcLogo.svg";
+import { Link } from "react-router-dom";
 import FollowButton from "../../../FollowButton";
 function PopularUserCard(props) {
   const { userName, accountName, id, isFollowed } = props;
   return (
     <div className={styles["container"]}>
-      <UserFakeAvatar className={styles["user-avatar"]} />
+      <Link to={`/user/${accountName}`}>
+        <UserFakeAvatar className={styles["user-avatar"]} />
+      </Link>
       <div className={styles["user-info"]}>
         <p className={styles["user-name"]}>
           {userName ? userName : "Pizza Hut"}
         </p>
         <p className={styles["user-account"]}>
-          {accountName ? accountName : "@pizzahut"}
+          {accountName ? `@${accountName}` : "@pizzahut"}
         </p>
       </div>
       <FollowButton currentUserID={1} id={id} isFollowed={isFollowed} />
--- src/Components/PopularBar/PopularUserList/index.jsx
+++ src/Components/PopularBar/PopularUserList/index.jsx
@@ -1,17 +1,53 @@
 import styles from "./PopularUserList.module.scss";
 import PopularUserCard from "./PopularUserCard";
+// 模擬user 假資料
+const fakeUser = [
+  {
+    id: 1,
+    account: "Rafael123",
+    name: "Rafael",
+    isFollowed: true,
+  },
+  {
+    id: 3,
+    account: "Natsu222",
+    name: "Natsu",
+    isFollowed: false,
+  },
+  {
+    id: 4,
+    account: "AnnW50",
+    name: "Ann",
+    isFollowed: true,
+  },
+  {
+    id: 5,
+    account: "Gina123",
+    name: "Gina",
+    isFollowed: false,
+  },
+  {
+    id: 6,
+    account: "Pop3",
+    name: "Pop11",
+    isFollowed: false,
+  },
+];
 function PopularUserList() {
   // 之後設定PopularUserCard以map渲染
   return (
     <div className={styles["container"]}>
       <h4 className={styles["popular__title"]}>推薦跟隨</h4>
       <div className={styles["popular__user-list"]}>
-        <PopularUserCard isFollowed={false} id={5} />
-        <PopularUserCard isFollowed={true} id={7} />
-        <PopularUserCard isFollowed={false} id={4} />
-        <PopularUserCard isFollowed={false} id={1} />
-        <PopularUserCard isFollowed={true} id={2} />
-        <PopularUserCard isFollowed={true} id={6} />
+        {fakeUser.map((user) => (
+          <PopularUserCard
+            key={user.id}
+            id={user.id}
+            accountName={user.account}
+            userName={user.name}
+            isFollowed={user.isFollowed}
+          />
+        ))}
       </div>
     </div>
   );
