ee04892a70f4fd7e4489e8088642ab33dfbfeead
yhosutun2491 <yhosutun2491@gmail.com>
2022-12-10 16:43:51 +0800

    feat- add assests/base scss/ react-router

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import styles from "./App.module.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className={styles["App"]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index></Route>
+            <Route path="login"></Route>
+            <Route path="register"></Route>
+            <Route path="setting"></Route>
+            <Route path="home"></Route>
+            <Route path="tweet/:id"></Route>
+            <Route path="/user/:username">
+              <Route index></Route>
+              <Route path="reply"></Route>
+              <Route path="likes"></Route>
+              <Route path="follower"></Route>
+              <Route path="following"></Route>
+            </Route>
+            <Route path="admin">
+              <Route index></Route>
+              <Route path="tweet-list"></Route>
+              <Route path="user-list"></Route>
+            </Route>
+          </Route>
+          <Route path="*" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
