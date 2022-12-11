30dd3ae10e876957ebc76630e97b7efac08065c4
yhosutun2491 <yhosutun2491@gmail.com>
2022-12-11 23:15:49 +0800

    feat- HomePage/UserTweetBox/UserTweeInput ok

--- src/Components/HomePage/index.jsx
+++ src/Components/HomePage/index.jsx
@@ -1,10 +1,24 @@
 import styles from "./HomePage.module.scss";
+import TweetInput from "./TweetInput";
+import UserTweetList from "./UserTweetList";
+import { useRef } from "react";
 function HomePage() {
+  const containerRef = useRef(null);
+  // 點擊置頂功能
+  function scrollTop() {
+    containerRef.current.scrollTo(0, 0);
+  }
   return (
-    <div className={styles["container"]}>
-      <div className={styles["page-title"]}>首頁</div>
-      <div className={styles["tweet-input"]}></div>
-      <div className={styles["tweet-list"]}></div>
+    <div className={styles["container"]} ref={containerRef}>
+      <div className={styles["page-title"]} onClick={scrollTop}>
+        首頁
+      </div>
+      <div className={styles["tweet-input"]}>
+        <TweetInput />
+      </div>
+      <div className={styles["tweet-list"]}>
+        <UserTweetList />
+      </div>
     </div>
   );
 }
