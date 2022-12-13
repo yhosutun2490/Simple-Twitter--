8067a1d42882b5af4647d0ed16bd96ded1017a2a
Natsu <natsu.tw0919@gmail.com>
2022-12-14 00:03:24 +0900

    feat- add style for RegisterPage

--- src/Pages/RegisterPage/index.jsx
+++ src/Pages/RegisterPage/index.jsx
@@ -1,9 +1,10 @@
 import { ReactComponent as AcLogo } from "../../assets/icons/AcLogo.svg";
 import Button from "../../Components/Button";
+import styles from "./RegisterPage.module.scss"
 
 function AuthInputAlert(prop) {
   const { alertMsg } = prop;
-  return <div>{alertMsg}</div>;
+  return <div className={styles["authinput-alertmsg"]}>{alertMsg}</div>;
 }
 
 function AuthInputWordCount(props) {
@@ -18,68 +19,73 @@ function AuthInputWordCount(props) {
 function AuthInput(props) {
   const { label, type, placeholder } = props;
   return (
-    <div>
-      <label>{label}</label>
-      <input type={type || "text"} placeholder={placeholder || ""} />
+    <div className={styles["authinput-box"]}>
+      <label className={styles["authinput-label"]}>{label}</label>
+      <input
+        className={styles["authinput"]}
+        type={type || "text"}
+        placeholder={placeholder || ""}
+      />
     </div>
   );
 }
 
 function RegisterPage() {
   return (
-    <div>
+    <div className={styles["container"]}>
       <div>
         <AcLogo />
       </div>
-      <h3>建立你的帳號</h3>
+      <h3 className={styles["title"]}>建立你的帳號</h3>
       {/* AuthInput group */}
-      <div>
-        <div>
+      <div className={styles["authinput-group"]}>
+        <div className={styles["authinput-container"]}>
           <AuthInput label="帳號" type="text" placeholder="請輸入帳號" />
-          <div>
+          <div className={styles["authinput-msg-box"]}>
             <AuthInputAlert alertMsg="字數超出上限！" />
             <AuthInputWordCount wordLength={10} wordLengthLimit={50} />
           </div>
         </div>
 
-        <div>
+        <div className={styles["authinput-container"]}>
           <AuthInput label="名稱" type="text" placeholder="請輸入使用者名稱" />
-          <div>
+          <div className={styles["authinput-msg-box"]}>
             <AuthInputAlert alertMsg="字數超出上限！" />
             <AuthInputWordCount wordLength={10} wordLengthLimit={50} />
           </div>
         </div>
 
-        <div>
+        <div className={styles["authinput-container"]}>
           <AuthInput label="Email" type="text" placeholder="請輸入Email" />
-          <div>
+          <div className={styles["authinput-msg-box"]}>
             <AuthInputAlert alertMsg="Email格式錯誤" />
           </div>
         </div>
 
-        <div>
+        <div className={styles["authinput-container"]}>
           <AuthInput label="密碼" type="text" placeholder="請設定密碼" />
-          <div>
+          <div className={styles["authinput-msg-box"]}>
             <AuthInputAlert alertMsg="密碼長度不夠！" />
           </div>
         </div>
 
-        <div>
+        <div className={styles["authinput-container"]}>
           <AuthInput
             label="密碼確認"
             type="text"
             placeholder="請再次輸入密碼"
           />
-          <div>
+          <div className={styles["authinput-msg-box"]}>
             <AuthInputAlert alertMsg="密碼不正確" />
           </div>
         </div>
       </div>
-      {/* Submit Button */}
-      <div>
+      {/* Register Button */}
+      <div className={styles["auth-button"]}>
         <Button styleName="lg-bg-logo">註冊</Button>
       </div>
-      <div>取消</div>
+      {/* Auth Link */}
+      <div className={styles["auth-link"]}>取消</div>
     </div>
   );
 }
