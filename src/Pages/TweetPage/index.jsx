b2c67ce9c49491e18fde2d7e933561784043d1a4
yhosutun2491 <yhosutun2491@gmail.com>
2022-12-13 09:02:40 +0800

    reafctor- TweePage can return to HomPage and ScrollTop

--- src/Pages/TweetPage/index.jsx
+++ src/Pages/TweetPage/index.jsx
@@ -2,14 +2,23 @@ import styles from "./TweetPage.module.scss";
 import { ReactComponent as PreArrow } from "../../assets/icons/arrowPre.svg";
 import TweetInfo from "../../Components/TweetInfo";
 import ReplyList from "../../Components/ReplyList";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
 function TweetPage(props) {
   const [isLiked, setIsLiked] = useState(false);
+  const containerRef = useRef(null);
+  // 點擊置頂功能
+  function scrollTop() {
+    containerRef.current.scrollTo(0, 0);
+  }
   const { replyerData } = props;
   return (
-    <div className={styles["container"]}>
+    <div className={styles["container"]} ref={containerRef} onClick={scrollTop}>
       <div className={styles["page-title-wrap"]}>
-        <PreArrow className={styles["arrow-img"]} />
+        <Link to={"/home"}>
+          <PreArrow className={styles["arrow-img"]} />
+        </Link>
+
         <p className={styles["page-title"]}>推文</p>
       </div>
       <div className={styles["tweet-info-wrap"]}>
