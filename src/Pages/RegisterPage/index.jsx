70f0a3202805cc29dd173950f4f867eeed297ce2
Natsu <natsu.tw0919@gmail.com>
2022-12-13 18:33:02 +0900

    feat- add RegisterPage component with fake alert message to check layout, import AC logo and Button

--- src/Pages/RegisterPage/index.jsx
+++ src/Pages/RegisterPage/index.jsx
@@ -1,3 +1,6 @@
+import { ReactComponent as AcLogo } from "../../assets/icons/AcLogo.svg";
+import Button from "../../Components/Button";
+
 function AuthInputAlert(prop) {
   const { alertMsg } = prop;
   return <div>{alertMsg}</div>;
@@ -21,3 +24,64 @@ function AuthInput(props) {
     </div>
   );
 }
+
+function RegisterPage() {
+  return (
+    <div>
+      <div>
+        <AcLogo />
+      </div>
+      <h3>建立你的帳號</h3>
+      {/* AuthInput group */}
+      <div>
+        <div>
+          <AuthInput label="帳號" type="text" placeholder="請輸入帳號" />
+          <div>
+            <AuthInputAlert alertMsg="字數超出上限！" />
+            <AuthInputWordCount wordLength={10} wordLengthLimit={50} />
+          </div>
+        </div>
+
+        <div>
+          <AuthInput label="名稱" type="text" placeholder="請輸入使用者名稱" />
+          <div>
+            <AuthInputAlert alertMsg="字數超出上限！" />
+            <AuthInputWordCount wordLength={10} wordLengthLimit={50} />
+          </div>
+        </div>
+
+        <div>
+          <AuthInput label="Email" type="text" placeholder="請輸入Email" />
+          <div>
+            <AuthInputAlert alertMsg="Email格式錯誤" />
+          </div>
+        </div>
+
+        <div>
+          <AuthInput label="密碼" type="text" placeholder="請設定密碼" />
+          <div>
+            <AuthInputAlert alertMsg="密碼長度不夠！" />
+          </div>
+        </div>
+
+        <div>
+          <AuthInput
+            label="密碼確認"
+            type="text"
+            placeholder="請再次輸入密碼"
+          />
+          <div>
+            <AuthInputAlert alertMsg="密碼不正確" />
+          </div>
+        </div>
+      </div>
+      {/* Submit Button */}
+      <div>
+        <Button styleName="lg-bg-logo">註冊</Button>
+      </div>
+      <div>取消</div>
+    </div>
+  );
+}
+
+export default RegisterPage;
