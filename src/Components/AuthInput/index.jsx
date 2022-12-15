3340441ca601e06d07cfd9e039161baf4f5f3487
Natsu <natsu.tw0919@gmail.com>
2022-12-15 17:02:15 +0900

    refactor- refactor AuthInput, AuthInputAlert, AuthInputWordCount into one component

--- src/Components/AuthInput/index.jsx
+++ src/Components/AuthInput/index.jsx
@@ -1,43 +1,47 @@
 import styles from "./AuthInput.module.scss";
 
 function AuthInput(props) {
-  const { label, type, value, placeholder, onChange, isAlert } = props;
+  const {
+    label,
+    type,
+    value,
+    placeholder,
+    onChange,
+    isAlert,
+    alertMsg,
+    isWordCount,
+    wordLength,
+    wordLengthLimit,
+  } = props;
   return (
-    <div
-      className={[
-        `${styles["authinput-box"]}`,
-        isAlert ? `${styles["error"]}` : ``,
-      ].join(" ")}
-    >
-      <label className={styles["authinput-label"]}>{label}</label>
-      <input
-        className={styles["authinput"]}
-        type={type || "text"}
-        value={value || ""}
-        placeholder={placeholder || ""}
-        onChange={(e) => onChange?.(e.target.value)}
-      />
+    <div className={["authinput-container"]}>
+      <div
+        className={[
+          `${styles["authinput-box"]}`,
+          isAlert ? `${styles["error"]}` : ``,
+        ].join(" ")}
+      >
+        <label className={styles["authinput-label"]}>{label}</label>
+        <input
+          className={styles["authinput"]}
+          type={type || "text"}
+          value={value || ""}
+          placeholder={placeholder || ""}
+          onChange={(e) => onChange?.(e.target.value)}
+        />
+      </div>
+      <div className={styles["authinput-msg-box"]}>
+        <div className={styles["authinput-alertmsg"]}>{alertMsg}</div>
+        {isWordCount ? (
+          <div>
+            {wordLength}/{wordLengthLimit}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
 
-// Input related message
-function AuthInputAlert(prop) {
-  const { alertMsg } = prop;
-  return <div className={styles["authinput-alertmsg"]}>{alertMsg}</div>;
-}
-
-function AuthInputWordCount(props) {
-  const { wordLength, wordLengthLimit } = props;
-  return (
-    <div>
-      {wordLength}/{wordLengthLimit}
-    </div>
-  );
-}
-
-export {
-  AuthInput,
-  AuthInputAlert,
-  AuthInputWordCount,
-};
+export default AuthInput
