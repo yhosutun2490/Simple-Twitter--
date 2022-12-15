4b8b28f0668922235eaaca663ed215714312c740
yhosutun2491 <yhosutun2491@gmail.com>
2022-12-15 10:46:39 +0800

    fix- resolve conflicts from updated  main (app.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
 import styles from "./App.module.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LayoutCommon from "./Components/LayoutCommon";
-import HomePage from "./Components/HomePage";
+import { AuthProvider } from "./Context/AuthContext"; //引入驗證用AuthProvider
+import HomePage from "./Pages/HomePage";
 import RegisterPage from "./Pages/RegisterPage";
 import LoginPage from "./Pages/LoginPage";
+import TweetPage from "./Pages/TweetPage";
 
 function App() {
   return (
     <div className={styles["App"]}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index></Route>
-            <Route path="login" element={<LoginPage />}></Route>
-            <Route path="register" element={<RegisterPage />}></Route>
-            <Route path="setting"></Route>
-            <Route path="home" element={<LayoutCommon />}>
-              <Route index element={<HomePage />}></Route>
-            </Route>
-            <Route path="tweet/:id"></Route>
-            <Route path="/user/:username">
+        <AuthProvider>
+          <Routes>
+            <Route path="/">
               <Route index></Route>
-              <Route path="reply"></Route>
-              <Route path="likes"></Route>
-              <Route path="follower"></Route>
-              <Route path="following"></Route>
+              <Route path="login" element={<LoginPage />}></Route>
+              <Route path="register" element={<RegisterPage />}></Route>
+              <Route path="setting"></Route>
+              <Route path="home" element={<LayoutCommon />}>
+                <Route index element={<HomePage />}></Route>
+              </Route>
+              <Route path="tweet/:id" element={<LayoutCommon />}>
+                <Route index element={<TweetPage />}></Route>
+              </Route>
+              <Route path="/user/:username">
+                <Route index></Route>
+                <Route path="reply"></Route>
+                <Route path="likes"></Route>
+                <Route path="follower"></Route>
+                <Route path="following"></Route>
+              </Route>
+              <Route path="admin">
+                <Route index></Route>
+                <Route path="tweet-list"></Route>
+                <Route path="user-list"></Route>
+              </Route>
             </Route>
-            <Route path="admin">
-              <Route index></Route>
-              <Route path="tweet-list"></Route>
-              <Route path="user-list"></Route>
-            </Route>
-          </Route>
-          <Route path="*" />
-        </Routes>
+            <Route path="*" />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
