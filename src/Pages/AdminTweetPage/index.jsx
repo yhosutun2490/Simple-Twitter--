20ea8d9eea4014fcd9944ecb8b977353474c9e03
Natsu <natsu.tw0919@gmail.com>
2022-12-17 00:56:03 +0900

    feat- add AdeminTweetPage component with fake datas to check layout

--- src/Pages/AdminTweetPage/index.jsx
+++ src/Pages/AdminTweetPage/index.jsx
@@ -21,3 +21,82 @@ function AdminTweetBox(props) {
   );
 }
 
+function AdminTweetPage() {
+  // fake data 待後端api測試檔通過後再次查看response格式 [Get]api/admin/tweets
+  // 是否該做分頁？設計稿上沒有指定
+  const datas = [
+    {
+      id: 11,
+      UserId: 2,
+      description: "voluptatibus iure quidem",
+      createdAt: "2020-09-20T16:41:19.000Z",
+      updatedAt: "2022-05-01T01:06:44.000Z",
+      userId: 2,
+      User: {
+        id: 2,
+        account: "user1",
+        name: "user1",
+        avatar:
+          "https://loremflickr.com/140/140/people/?random=70.97604245559303",
+        cover:
+          "https://loremflickr.com/639/200/image/?random=78.61165147050005",
+      },
+    },
+    {
+      id: 11,
+      UserId: 2,
+      description: "voluptatibus iure quidem",
+      createdAt: "2020-09-20T16:41:19.000Z",
+      updatedAt: "2022-05-01T01:06:44.000Z",
+      userId: 2,
+      User: {
+        id: 2,
+        account: "user1",
+        name: "user1",
+        avatar:
+          "https://loremflickr.com/140/140/people/?random=70.97604245559303",
+        cover:
+          "https://loremflickr.com/639/200/image/?random=78.61165147050005",
+      },
+    },
+    {
+      id: 11,
+      UserId: 2,
+      description: "voluptatibus iure quidem",
+      createdAt: "2020-09-20T16:41:19.000Z",
+      updatedAt: "2022-05-01T01:06:44.000Z",
+      userId: 2,
+      User: {
+        id: 2,
+        account: "user1",
+        name: "user1",
+        avatar:
+          "https://loremflickr.com/140/140/people/?random=70.97604245559303",
+        cover:
+          "https://loremflickr.com/639/200/image/?random=78.61165147050005",
+      },
+    },
+  ];
+
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["title"]}>推文清單</div>
+      <div className={styles["tweet-list"]}>
+        {datas &&
+          datas.map((data) => (
+            <AdminTweetBox
+              key={data.id}
+              tweeterAccount={data.User.account}
+              tweeterName={data.User.name}
+              avatar={data.User.avatar}
+              update={data.createdAt}
+              content={data.description}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default AdminTweetPage;
+
