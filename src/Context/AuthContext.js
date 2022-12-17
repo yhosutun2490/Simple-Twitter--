a53334cc2e8fa5787c7f804dce153674366dfb4f
Natsu <natsu.tw0919@gmail.com>
2022-12-18 01:51:48 +0900

    feat- add register property into defaultValue for context and add login function to context.

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,13 +1,14 @@
-import { createContext} from "react";
-import { getAllTweets } from "../Api/TweetAPI" // 引入Tweet系列api
+import { createContext } from "react";
 // import { useEffect } from "react";
 // import { useLocation } from "react-router-dom";
 import { useContext } from "react";
+import { login } from '../Api/AuthAPI'
 
 // 設定一開始context 預設值
 const defaultValue = {
   isAuthenticated: false, //是否通過後端驗證
   currentMember: null,  //現在使用者是誰
+  register: null, //註冊function
   login: null, //登入function
   logout: null, // 登出function
 }
@@ -16,20 +17,28 @@ const AuthContext = createContext(defaultValue)
 // useContext hook 讓子層可以調用
 const useAuth = () => useContext(AuthContext)
 
-function AuthProvider (props) {
+function AuthProvider(props) {
   // 先放兩個狀態，一個是登入驗證是否還有效，一個拿來裝payload使用者資料
-  // const [isAuth,setIsAuth] = useState(false)
-  // const [payload, setPayload] = useState(null)
-  
+  // const [isAuthenticated, setIsAuthenticated] = useState(false)
+  // const [payload, setPayload] = useState(false)
   return (
     <AuthContext.Provider value={
       {
-      getAllTweets: getAllTweets
-    }
+        login: async (data) => {
+          const { success, token } = await login({
+            account: data.accountTrimmed,
+            password: data.passwordTrimmed,
+          });
+          if (token) {
+            localStorage.setItem('authToken', token);
+          }
+          return success;
+        },
+      }
     }>
       {props.children}
     </AuthContext.Provider>
   )
 }
 
-export  {AuthProvider,useAuth}
+export { AuthProvider, useAuth }
