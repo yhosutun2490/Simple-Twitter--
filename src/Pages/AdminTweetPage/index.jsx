b98a298a1c7044deb3c765f0d91ec2eb3a362e6c
Natsu <natsu.tw0919@gmail.com>
2022-12-19 15:25:28 +0900

    feat- replace fake data with get all tweets api function, wrapped in the useEffect

--- src/Pages/AdminTweetPage/index.jsx
+++ src/Pages/AdminTweetPage/index.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ReactComponent as AdminDeleteIcon } from "../../assets/icons/admin_delete_icon.svg";
 import UserInfo from "../../Components/UserTweetBox/UserInfo";
 import styles from "./AdminTweetPage.module.scss";
 import { TimeFromNow } from "../../CostumHook/TransFormDate";
+import { adminGetAllTweets } from "../../Api/AdminAPI";
 
 function AdminTweetBox(props) {
   //須從後端傳入的資料
@@ -43,62 +44,7 @@ function AdminTweetBox(props) {
 }
 
 function AdminTweetPage() {
-  // fake data 待後端api測試檔通過後再次查看response格式 [Get]api/admin/tweets
-  // 是否該做分頁待討論，設計稿上沒有指定
-  const fakeTweetList = [
-    {
-      id: 11,
-      UserId: 2,
-      description: "voluptatibus iure quidem",
-      createdAt: "2020-09-20T16:41:19.000Z",
-      updatedAt: "2022-05-01T01:06:44.000Z",
-      userId: 2,
-      User: {
-        id: 2,
-        account: "user1",
-        name: "user1",
-        avatar:
-          "https://loremflickr.com/140/140/people/?random=70.97604245559303",
-        cover:
-          "https://loremflickr.com/639/200/image/?random=78.61165147050005",
-      },
-    },
-    {
-      id: 12,
-      UserId: 2,
-      description: "voluptatibus iure quidem",
-      createdAt: "2020-09-20T16:41:19.000Z",
-      updatedAt: "2022-05-01T01:06:44.000Z",
-      userId: 2,
-      User: {
-        id: 2,
-        account: "user1",
-        name: "user1",
-        avatar:
-          "https://loremflickr.com/140/140/people/?random=70.97604245559303",
-        cover:
-          "https://loremflickr.com/639/200/image/?random=78.61165147050005",
-      },
-    },
-    {
-      id: 13,
-      UserId: 2,
-      description: "voluptatibus iure quidem",
-      createdAt: "2020-09-20T16:41:19.000Z",
-      updatedAt: "2022-05-01T01:06:44.000Z",
-      userId: 2,
-      User: {
-        id: 2,
-        account: "user1",
-        name: "user1",
-        avatar:
-          "https://loremflickr.com/140/140/people/?random=70.97604245559303",
-        cover:
-          "https://loremflickr.com/639/200/image/?random=78.61165147050005",
-      },
-    },
-  ];
-  const [tweetList, setTweetList] = useState(fakeTweetList);
+  const [tweetList, setTweetList] = useState([]);
 
   //Delete function
   //  const handleDelete = async (id) => {
@@ -119,6 +65,19 @@ function AdminTweetPage() {
       return preTweetList.filter((tweet) => tweet.id !== id);
     });
   };
+
+  useEffect(() => {
+    const getAllTweetsAsync = async () => {
+      try {
+        const data = await adminGetAllTweets();
+        setTweetList(data);
+        console.log(data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllTweetsAsync();
+  }, []);
 
   return (
     <div className={styles["container"]}>
