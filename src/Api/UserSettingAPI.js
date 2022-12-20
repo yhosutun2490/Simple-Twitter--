9ae174d253b15f107669eeb97a7c89399303ed64
Natsu <natsu.tw0919@gmail.com>
2022-12-21 06:52:05 +0900

    feat- add set user data function

---
+++ src/Api/UserSettingAPI.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const baseUrl = "https://floating-forest-88499.herokuapp.com/api/users";
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
+
+export const setUserData = async (payload) => {
+  const { id, account, name, email, password, checkPassword } = payload
+  try {
+    const { data } = await axiosInstance.put(`${baseUrl}/${id}`, {
+      id, account, name, email, password, checkPassword
+    });
+
+    if (data) {
+      return { success: true, ...data };
+    }
+
+    return data;
+  } catch (error) {
+    // 待後端
+    console.error("[Set User Failed]:", error);
+    //const errMsg = error.response.data.message
+    //return errMsg
+  }
+};
