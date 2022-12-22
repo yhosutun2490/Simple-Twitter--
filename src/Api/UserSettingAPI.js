9694aef5f617d3d54c4d33f79564af0131074c84
Natsu <natsu.tw0919@gmail.com>
2022-12-22 18:15:35 +0900

    feat- add error message by statsu code for set user data function

--- src/Api/UserSettingAPI.js
+++ src/Api/UserSettingAPI.js
@@ -32,9 +32,35 @@ export const setUserData = async (payload) => {
 
     return data;
   } catch (error) {
-    // 待後端
     console.error("[Set User Failed]:", error);
-    //const errMsg = error.response.data.message
-    //return errMsg
+    const errCode = error.response.data.status
+    let errMsg = ''
+
+    switch(errCode) {
+      case '422':
+        errMsg = '密碼長度不符或是確認密碼不相符'
+        break
+      case '403':
+        errMsg = '名稱字數超過上限'
+        break
+      case '401':
+        errMsg = 'email格式不符'
+        break
+      case '408':
+        errMsg = 'Email已重複註冊'
+        break
+      case '423':
+        errMsg = '帳號已重複註冊'
+        break
+      // 錯誤code重複待後端修正
+      // case '401':
+      //   return '您尚未登入'
+      case '500':
+        errMsg = '伺服器發生錯誤'
+        break
+      default:
+        errMsg = '發生未預期錯誤...'
+    }
+    return { success: false, errMsg: errMsg}
   }
 };
